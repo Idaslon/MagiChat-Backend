9a65c67375a6a561fa3a6618496ca353da9319f5
DestroyeerU <destroyeeru18@gmail.com>
2021-01-03 21:06:05 -0300

    babel plugins bug fix

--- babel.config.js
+++ babel.config.js
@@ -11,27 +11,25 @@ module.exports = {
     '@babel/preset-typescript',
   ],
   plugins: [
-    [
-      ["@babel/plugin-proposal-decorators", { "legacy": true }],
+    ['@babel/plugin-proposal-decorators', { 'legacy': true }],
+    ['@babel/plugin-proposal-class-properties', { 'loose': true }],
 
-      'module-resolver',
-      {
-        alias: {
-          '@controllers': './src/app/controllers',
-          '@middlewares': './src/app/middlewares',
-          '@errors': './src/app/errors',
-          '@utils': './src/app/utils',
+    ['module-resolver', {
+      alias: {
+        '@controllers': './src/app/controllers',
+        '@middlewares': './src/app/middlewares',
+        '@errors': './src/app/errors',
+        '@utils': './src/app/utils',
 
-          '@entity': './src/database/entity',
-          '@schemas': './src/database/schemas',
+        '@entity': './src/database/entity',
+        '@schemas': './src/database/schemas',
 
-          '@mytypes': './src/types',
-          '@services': './src/services',
-          '@config': './src/config',
-          '@': './src',
-        },
+        '@mytypes': './src/types',
+        '@services': './src/services',
+        '@config': './src/config',
+        '@': './src',
       },
-    ],
+    }],
   ],
   ignore: ['**/*/.test.ts', '**/*/.test.js', './src/types', '**/*.d.ts'],
 };
